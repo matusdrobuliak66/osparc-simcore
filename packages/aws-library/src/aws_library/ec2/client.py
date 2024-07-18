5b97719cf65c7e6f1584694c27c4c8dda773a90e
matusdrobuliak66 <drobuliak66@gmail.com>
2024-07-18 13:13:53 +0200

    Revert "🐛♻️Clusters-keeper: use private dns name instead of public IP (#5883)"
    This reverts commit a93de841704db21fd2602813bf19138d696790b5.

--- packages/aws-library/src/aws_library/ec2/client.py
+++ packages/aws-library/src/aws_library/ec2/client.py
@@ -199,7 +199,11 @@ class SimcoreEC2API:
                     launch_time=instance["LaunchTime"],
                     id=instance["InstanceId"],
                     aws_private_dns=instance["PrivateDnsName"],
-                    aws_public_ip=instance.get("PublicIpAddress", None),
+                    aws_public_ip=(
+                        instance["PublicIpAddress"]
+                        if "PublicIpAddress" in instance
+                        else None
+                    ),
                     type=instance["InstanceType"],
                     state=instance["State"]["Name"],
                     tags=parse_obj_as(
@@ -259,7 +263,11 @@ class SimcoreEC2API:
                         launch_time=instance["LaunchTime"],
                         id=instance["InstanceId"],
                         aws_private_dns=instance["PrivateDnsName"],
-                        aws_public_ip=instance.get("PublicIpAddress", None),
+                        aws_public_ip=(
+                            instance["PublicIpAddress"]
+                            if "PublicIpAddress" in instance
+                            else None
+                        ),
                         type=instance["InstanceType"],
                         state=instance["State"]["Name"],
                         resources=ec2_instance_types[0].resources,
